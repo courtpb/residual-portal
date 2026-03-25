08b87bd302d2ae754ef727fc895316434dbe7582
courtpb <courtney@posbrokers.com>
2026-03-25 10:07:45 -0700

    Create users.js

---
+++ users.js
@@ -0,0 +1,16 @@
+const USERS = [
+  {
+    email: "admin@email.com",
+    password: "admin123",
+    role: "admin",
+    repCodes: [],
+    split: 0
+  },
+  {
+    email: "rep1@email.com",
+    password: "rep123",
+    role: "rep",
+    repCodes: ["08040-004","08040-005"],
+    split: 50
+  }
+];
